883f825173379753826e961df82960df06b635c9
ruslan-alybaev <ruslanalybaev7213@gmail.com>
2023-11-29 17:32:00 -0800

    second commit

--- bin/lesson_16_practice.dart
+++ bin/lesson_16_practice.dart
@@ -1,11 +1,59 @@
 import 'dart:io';
 
+
 void main() {
   // print(appNalog());
   // print(zapros());
-  print(type());
+  // print(type());
+
+  double balance = 0;
+  menu(balance);
 }
 
+void menu (double balance) {
+  print("""Добрый день!
+  Добро пожаловать в личный кабинет!
+  Выберите операцию
+  1) Депозит
+  2) Снять деньги
+  3) Запрос баланса
+  4) Закрыть счёт""");
+  String choice = stdin.readLineSync() ?? "";
+  switch (choice) {
+    case "1":
+    print("Введите сумму депозита");
+    double inputBalance = double.tryParse(stdin.readLineSync() ?? "") ?? 0;
+      balance = deposite(balance, inputBalance);
+      menu(balance);
+      break;
+      case "2":
+      print("Введите сумму снятия");
+      double outBalance = double.tryParse(stdin.readLineSync() ?? "") ?? 0;
+      balance = cashOut(balance, outBalance);
+      menu(balance);
+      break;
+      case "3":
+      print("Ваш баланс - $balance");
+      menu(balance);
+      break;
+      case "4":
+      print("Счет закрыт");
+      break;
+      default:
+      print("Error");
+      menu(balance);
+      break;
+  }
+}
+double deposite (double currentBalance, inputBalance) {
+  return currentBalance + inputBalance;
+} 
+double cashOut (double currentBalance, outBalance) {
+  return currentBalance - outBalance;
+} 
+
+
+// Задача №1
 // String appNalog () {
 //  print("Введите сумму дохода");
 //   int summ = int.tryParse(stdin.readLineSync() ?? "") ?? 0;
@@ -22,6 +70,7 @@ void main() {
 // }
 
 
+// Задача №2
 // double zapros () {
 //   print("Введите первое число");
 //   double num1 = double.tryParse(stdin.readLineSync() ?? "") ?? 0;
@@ -50,20 +99,20 @@ void main() {
 // }
 
 
-String type () {
-  print("Введите расширение (.txt, .jpg, .pdf)");
-  String input = stdin.readLineSync() ?? "";
-
-  switch (input) {
-    case ".txt":
-    return "текстовый";
-    case ".jpg":
-    return "изображение";
-    case ".pdf":
-    return "документ";
-  }
-  return "Неправильный формат";
-}
+// Задача №3
+// String type () {
+//   print("Введите расширение (.txt, .jpg, .pdf)");
+//   String input = stdin.readLineSync() ?? "";
+//   switch (input) {
+//     case ".txt":
+//     return "текстовый";
+//     case ".jpg":
+//     return "изображение";
+//     case ".pdf":
+//     return "документ";
+//   }
+//   return "Неправильный формат";
+// }
 
 
 
